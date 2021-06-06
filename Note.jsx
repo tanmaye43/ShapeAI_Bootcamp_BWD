dd01628d0a30dd6cf3cbd0d86de1dda9b2938d21
tanmaye43 <84314472+tanmaye43@users.noreply.github.com>
2021-06-06 11:16:19 +0600

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>Javascript and React.js</h1>
+<p>This was an amazing bootcamp taken up tanmaye kudupudi we cover everything from scratch including  and react.js,HTML.
+</p>
+    </div>
+  );
+}
+
+export default Note;
